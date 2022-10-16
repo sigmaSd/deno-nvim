93576102e4f823a514d7377c01b6798a6805310c
Nbiba Bedis <bedisnbiba@gmail.com>
2022-10-16 15:49:21 +0100

    add default registers

--- lua/deno-nvim/config.lua
+++ lua/deno-nvim/config.lua
@@ -3,16 +3,25 @@ local M = {}
 local defaults = {
     -- all the opts to send to nvim-lspconfig
     -- these override the defaults set by deno-nvim
-    -- see TODO
+    -- the defaults comes from https://github.com/denoland/vscode_deno/blob/main/package.json
     server = {
         settings = {
             deno = {
                 enable = true,
                 unstable = true,
-            }
+                suggest = {
+                    imports = {
+                        hosts = {}
+                    }
+                }
+            },
         }
     }, -- deno lsp options
 }
+
+for _, register in ipairs({ "https://deno.land", "https://x.nest.land", "https://crux.land" }) do
+    defaults.server.settings.deno.suggest.imports.hosts[register] = true
+end
 
 function M.setup(options)
     M.options = vim.tbl_deep_extend("force", {}, defaults, options or {})
