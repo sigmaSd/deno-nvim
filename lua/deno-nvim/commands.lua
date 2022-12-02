d302ec9ec26713e0b6d477bc213618520641de28
Nbiba Bedis <bedisnbiba@gmail.com>
2022-12-02 19:36:35 +0100

    Run the test with the exact name

--- lua/deno-nvim/commands.lua
+++ lua/deno-nvim/commands.lua
@@ -3,11 +3,11 @@ local M = {}
 function M.setup_lsp_commands()
     vim.lsp.commands["deno.test"] = function(args)
         local file = args.arguments[1]:gsub("file:///", "/")
-        local test_name = args.arguments[2]
+        local test_name = "'/^" .. args.arguments[2] .. "$/'"
         local maybe_debug = args.title == "Debug" and "--inspect-brk " or ""
 
         local cmd = "split term://deno test --no-check --unstable -A " ..
-            maybe_debug .. file .. " --filter " .. test_name
+            maybe_debug .. file .. " --filter " .. vim.fn.fnameescape(test_name)
         vim.cmd(cmd)
     end
 end
